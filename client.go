bd2cc9587d13e5d363a51b98a29a709ab55020ec
azat valiev <azvaliev@gmail.com>
2022-08-13 15:57:10 -0500

    Consistent formatting. Moving text from input to preview

---
+++ client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  godom "github.com/azvaliev/godom/document"
+)
+
+func main() {
+  dom := godom.DOM{}
+  dom.Init()
+
+  // Get the textarea with md input
+  textarea, err := dom.QuerySelector(".editing-area > textarea")
+  if err != nil {
+    godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
+    return
+  }
+
+  // Check that text area was selected
+  if !textarea.Exists() {
+    godom.LogError("Textarea not found, exiting")
+    return
+  }
+
+  // Get the preview area
+  preview, err := dom.QuerySelector(".preview-area")
+  if err != nil {
+    godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
+    return
+  }
+  if !preview.Exists() {
+    godom.LogError("Preview area not found, exiting")
+    return
+  }
+
+  // Listen for textarea changes
+  textarea.AddEventListener("keyup", func(this *godom.Element, e godom.Event) {
+    // Get the textarea value
+    text, err := textarea.GetAttribute("value")
+    if err != nil {
+      godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
+      return
+    }
+    // Set the preview area value
+    preview.SetAttribute("innerHTML", text)
+  })
+
+  <-make(chan bool)
+}
