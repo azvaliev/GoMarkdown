4c48fc0c1fa8fd2484105510b155de2e69f8fa5b
azat valiev <azvaliev@gmail.com>
2022-08-13 18:39:38 -0500

    Added newline functionality. Two lines of return, or two spaces at end of line and single return

--- client.go
+++ client.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-  godom "github.com/azvaliev/godom/document"
+	godom "github.com/azvaliev/godom/document"
 )
 
 func main() {
-  dom := godom.DOM{}
-  dom.Init()
+	dom := godom.DOM{}
+	dom.Init()
 
-  // Get the textarea with md input
-  textarea, err := dom.QuerySelector(".editing-area > textarea")
-  if err != nil {
-    godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
-    return
-  }
+	// Get the textarea with md input
+	textarea, err := dom.QuerySelector(".editing-area > textarea")
+	if err != nil {
+		godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
+		return
+	}
 
-  // Check that text area was selected
-  if !textarea.Exists() {
-    godom.LogError("Textarea not found, exiting")
-    return
-  }
+	// Check that text area was selected
+	if !textarea.Exists() {
+		godom.LogError("Textarea not found, exiting")
+		return
+	}
 
-  // Get the preview area
-  preview, err := dom.QuerySelector(".preview-area")
-  if err != nil {
-    godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
-    return
-  }
-  if !preview.Exists() {
-    godom.LogError("Preview area not found, exiting")
-    return
-  }
+	// Get the preview area
+	preview, err := dom.QuerySelector(".preview-area")
+	if err != nil {
+		godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
+		return
+	}
+	if !preview.Exists() {
+		godom.LogError("Preview area not found, exiting")
+		return
+	}
 
-  // Listen for textarea changes
-  textarea.AddEventListener("keyup", func(this *godom.Element, e godom.Event) {
-    // Get the textarea value
-    text, err := textarea.GetAttribute("value")
-    if err != nil {
-      godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
-      return
-    }
-    
-    // Format the text
-    result := Format(text)
+	// Listen for textarea changes
+	textarea.AddEventListener("input", func(this *godom.Element, e godom.Event) {
+		// Get the textarea value
+		text, err := this.GetAttribute("value")
+		if err != nil {
+			godom.LogErrorf("%s\n%s", err.Msg, err.Stacktrace)
+			return
+		}
 
-    // Set the preview area value
-    preview.SetAttribute("innerHTML", result)
-  })
+		// Format the text
+		result := Format(text)
 
-  <-make(chan bool)
+		// Set the preview area value
+		preview.SetAttribute("innerHTML", result)
+	})
+
+	<-make(chan bool)
 }
--- format.go
+++ format.go
@@ -3,25 +3,33 @@ package main
 import (
 	"fmt"
 	"regexp"
+
+	"html/template"
 )
 
 type MdRegExps struct {
+	NewLine *regexp.Regexp
 	Headers *regexp.Regexp
 }
 
 var fmtRegExps = MdRegExps{
-  Headers: regexp.MustCompile(`(?mi)(#{1,6}) ([^\n]*)`),
+	NewLine: regexp.MustCompile(`(?m)^([^#|\n]*)(?:\n| {2})$\n`),
+	Headers: regexp.MustCompile(`(?mi)(#{1,6}) ([^\n]+)`),
 }
 
 func Format(raw string) string {
-  var formatted string
+	sanitizedRaw := template.HTMLEscapeString(raw)
+	var formatted string
 
-  // Format the raw text with headers
-  formatted = fmtRegExps.Headers.ReplaceAllStringFunc(raw, func(match string) string {
-    parts := fmtRegExps.Headers.FindAllStringSubmatch(match, 1)[0]
-    headerLevel := len(parts[1])
-    return fmt.Sprintf("<h%d>%s</h%d>", headerLevel, parts[2], headerLevel)
-  })
+	// Add newlines
+	formatted = fmtRegExps.NewLine.ReplaceAllString(sanitizedRaw, "$1<br>")
+
+	// Add headers
+	formatted = fmtRegExps.Headers.ReplaceAllStringFunc(formatted, func(match string) string {
+		parts := fmtRegExps.Headers.FindAllStringSubmatch(match, 1)[0][1:]
+		level, content := len(parts[0]), parts[1]
+		return fmt.Sprintf("<h%d>%s</h%d>", level, content, level)
+	})
 
 	return formatted
 }
