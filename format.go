6729b57d5cb0e7bea015e1fa570dc8c00f20ffd6
azat valiev <azvaliev@gmail.com>
2022-08-13 20:32:24 -0500

    Added support for **bold** or __bold__

--- format.go
+++ format.go
@@ -8,21 +8,24 @@ import (
 )
 
 type MdRegExps struct {
-	NewLine *regexp.Regexp
-	Headers *regexp.Regexp
+	NewLine, Headers, Bold *regexp.Regexp
 }
 
 var fmtRegExps = MdRegExps{
 	NewLine: regexp.MustCompile(`(?m)^([^#|\n]*)(?:\n| {2})$\n`),
 	Headers: regexp.MustCompile(`(?mi)(#{1,6}) ([^\n]+)`),
+	Bold:    regexp.MustCompile(`(?m)(?:\*\*(.+)\*\*)|(?:(\W)__(.+)__(\W))`),
 }
 
 func Format(raw string) string {
 	sanitizedRaw := template.HTMLEscapeString(raw)
 	var formatted string
 
+	// Add bold
+	formatted = fmtRegExps.Bold.ReplaceAllString(sanitizedRaw, "$2<b>$1$3</b>$4")
+
 	// Add newlines
-	formatted = fmtRegExps.NewLine.ReplaceAllString(sanitizedRaw, "$1<br>")
+	formatted = fmtRegExps.NewLine.ReplaceAllString(formatted, "$1<br>")
 
 	// Add headers
 	formatted = fmtRegExps.Headers.ReplaceAllStringFunc(formatted, func(match string) string {
