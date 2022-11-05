856f27628884f7e5efd0073542be5fd06016b6f8
azat valiev <azvaliev@gmail.com>
2022-11-05 18:28:12 -0500

    Supporting ordered and unordered lists

--- format.go
+++ format.go
@@ -9,19 +9,21 @@ import (
 )
 
 type MdRegExps struct {
-	Bold, Italic, LineBreak, Paragraph, Header, Link *regexp.Regexp
+	Bold, Italic, List, LineBreak, Paragraph, Header, Link *regexp.Regexp
 }
 
 var fmtRegExps = MdRegExps{
 	Bold:      regexp.MustCompile(`(?m)(?:\*\*((?:\n[^\n]|[^*])+)\*\*)|(?:(\b_?)__((?:\n[^\n]|[^_])+)__(_?\b))`),
 	Italic:    regexp.MustCompile(`(?m)(?:\*((?:\n[^\n]|[^*])+)\*)|(?:(\b)_((?:\n[^\n]|[^_])+)_(\b))`),
+	List:      regexp.MustCompile(`(?m)((?:-|\d)(?:[^\n]|\n[^#\n])*)`),
 	LineBreak: regexp.MustCompile(`(?m)^([^#\n])([^\n]*)(?: {2})$\n`),
 	Paragraph: regexp.MustCompile(`(?m)^((?:<[^h>|<h[^\d])|[^#\n])((?:\n[^\n]|<[^h]|<h[^\d]|[^h]\d|[^<][h][\d]|[^\d<\n])*)(?:\n\n|\n?\z|\n(<[h]))`),
-	Header:   regexp.MustCompile(`(?m)(#{1,6}) ([^\n]+)`),
-  Link: regexp.MustCompile(`(?m)\[([^\n\]]+)\]\((#[^\n)]+|https?://[^\n)]+)\)`),
+	Header:    regexp.MustCompile(`(?m)(#{1,6}) ([^\n]+)`),
+	Link:      regexp.MustCompile(`(?m)\[([^\n\]]+)\]\((#[^\n)]+|https?://[^\n)]+)\)`),
 }
 
-var linkSplitter = regexp.MustCompile(`(?m)(#|http)`)
+var linkSplitter *regexp.Regexp = regexp.MustCompile(`(?m)(#|http)`)
+var listSplitter *regexp.Regexp = regexp.MustCompile(`(?m)(?:-|\d.) ([^\n]*)`)
 
 func Format(raw string) string {
 	sanitizedRaw := template.HTMLEscapeString(raw)
@@ -32,6 +34,17 @@ func Format(raw string) string {
 
 	// Add italic
 	formatted = fmtRegExps.Italic.ReplaceAllString(formatted, "$2<i>$1$3</i>$4")
+
+	// Add lists
+	formatted = fmtRegExps.List.ReplaceAllStringFunc(formatted, func(match string) string {
+		// unordered list
+		if strings.HasPrefix(match, "-") {
+			return `<ul>` + listSplitter.ReplaceAllString(match, "<li>$1</li>") + `</ul>`
+		}
+
+		// ordered list
+		return `<ol>` + listSplitter.ReplaceAllString(match, "<li>$1</li>") + `</ol>`
+	})
 
 	// Add newlines
 	formatted = fmtRegExps.LineBreak.ReplaceAllString(formatted, "$1$2<br />")
@@ -46,19 +59,16 @@ func Format(raw string) string {
 	// Add paragraphs
 	formatted = fmtRegExps.Paragraph.ReplaceAllString(formatted, "<p>$1$2</p>$3")
 
-  formatted = fmtRegExps.Link.ReplaceAllStringFunc(formatted, func (match string) string {
-    parts := linkSplitter.Split(match, 2)
-    text, link := parts[0], parts[1]
-    fmt.Println(text, link)
+	formatted = fmtRegExps.Link.ReplaceAllStringFunc(formatted, func(match string) string {
+		parts := linkSplitter.Split(match, 2)
+		text, link := parts[0], parts[1]
 
-    if (strings.Contains(link, "://") && !strings.HasPrefix(link, "http")) {
-      return `<a href="http` + link[0:len(link) -1] + `" rel="noopener noreferrer" target="_blank">` + text[1:len(text) -2] + `</a>`
-    }
+		if strings.Contains(link, "://") && !strings.HasPrefix(link, "http") {
+			return `<a href="http` + link[0:len(link)-1] + `" rel="noopener noreferrer" target="_blank">` + text[1:len(text)-2] + `</a>`
+		}
 
-    return `<a href="#` + link[0:len(link) - 1] + `" rel="noopener noreferrer">` + text[1:len(text) -2] + `</a>`
-  })
-
-  fmt.Println(formatted);
+		return `<a href="#` + link[0:len(link)-1] + `" rel="noopener noreferrer">` + text[1:len(text)-2] + `</a>`
+	})
 
 	return formatted
 }
